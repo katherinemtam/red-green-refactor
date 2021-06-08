2e6b9fb97ebb20a0362ed9d3a9270cd449bc26f2
Katherine Tam <katherinemtam@gmail.com>
2021-06-07 17:08:30 -0700

    copyAndPush test

--- red-green-refactor.test.js
+++ red-green-refactor.test.js
@@ -13,3 +13,11 @@ describe('getName function', () => {
   });
 });
 
+describe('copyAndPush function', () => {
+  test('returns a new array with all the items in the original array and a new item pushed to the end', () => {
+    const numbers = [1, 2, 3];
+    const newArr = copyAndPush(numbers, 4);
+
+    expect(newArr).toEqual([1, 2, 3, 4]);
+  });
+});
